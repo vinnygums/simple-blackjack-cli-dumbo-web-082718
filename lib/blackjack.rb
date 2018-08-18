629e19ad2abfcefaa24cd8df69f7eccf8949225c
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-18 16:01:09 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,8 +37,8 @@ end
 
 def hit?(card_total)
   prompt_user
-  get_user_input
-  if get_user_input == "s"
+  input = get_user_input
+  if input == "s"
     card_total
   elsif get_user_input == "h"
     card_total += deal_card
